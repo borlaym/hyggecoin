9ea00fe48a541beb09d55963ba660bdd601c71ca
Marton Borlay <borlaym@gmail.com>
2021-03-15 12:40:38 +0100

    read firebase config from env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
   databaseURL: 'https://hyggecoin-default-rtdb.europe-west1.firebasedatabase.app/'
 });
 
