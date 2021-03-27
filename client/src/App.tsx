7a0d2fabca1489dba4360d7427c85f949e8bc541
Marton Borlay <borlaym@gmail.com>
2021-03-27 16:33:16 +0100

    fix react, add layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import BlockList from './components/BlockList';
 import DataProvider from './components/DataProvider';
+import Layout from './components/Layout';
 
 export default function App() {
 
   return (
     <DataProvider>
-      <BlockList />
+      <Layout />
     </DataProvider>
   )
 }
