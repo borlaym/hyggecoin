877cb73f354eac321808e79af2ac533a49f065b9
Marton Borlay <borlaym@gmail.com>
2021-03-27 21:25:00 +0100

    cards for blocks

--- client/src/components/Block.tsx
+++ client/src/components/Block.tsx
@@ -1,15 +1,52 @@
+import { makeStyles, Card, CardContent, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
 import React from 'react';
 import { Block } from '../../../src/block';
 import { Transaction } from '../../../src/transaction';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 type Props = {
-  block: Block<Transaction[]>
+  block: Block<Transaction[]>;
+  index: number;
 }
 
-export default function BlockComponent({ block }: Props) {
+const useStyles = makeStyles({
+  hash: {
+    fontSize: 12
+  }
+});
+
+export default function BlockComponent({ block, index }: Props) {
+  const classes = useStyles();
   return (
-    <div>
-      <p>Hash: {block.hash}</p>
-    </div>
+    <Card>
+      <CardContent>
+        <Typography component="h2" variant="h6" color="textSecondary" gutterBottom>
+          Block {index}
+        </Typography>
+        <Typography color="textSecondary" className={classes.hash}>
+          {block.hash}
+        </Typography>
+        <Typography color="textSecondary" className={classes.hash} gutterBottom>
+          {new Date(block.timestamp).toLocaleString()}
+        </Typography>
+
+      </CardContent>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography color="textSecondary">Transactions</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              TODO list transactions here
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+    </Card>
   )
 }
--- client/src/components/BlockList.tsx
+++ client/src/components/BlockList.tsx
@@ -1,10 +1,20 @@
-import React, { useContext } from "react";
+import { Grid } from "@material-ui/core";
+import React, { useContext, useMemo } from "react";
 import BlockComponent from "./Block";
 import { DataContext } from "./DataProvider";
 
 export default function BlockList() {
   const chain = useContext(DataContext);
-  return <>
-    {chain.map(block => <BlockComponent block={block} key={block.hash} />)}
-  </>
+
+  const reversedChain = useMemo(() => [...chain].reverse(), [chain]);
+
+  return (
+    <Grid container spacing={2}>
+      {reversedChain.map((block, i) => (
+        <Grid item lg={6}>
+          <BlockComponent block={block} key={block.hash} index={reversedChain.length - i} />
+        </Grid>
+      ))}
+    </Grid>
+  )
 }
